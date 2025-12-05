eadf40fc8cc17b7aa91c5b290e152a476c0f6a7c
rdominguez-369 <rdominguez@pucp.pe>
2025-12-05 10:56:00 +0100

    Commit inicial

---
+++ Personaje.java
@@ -0,0 +1,15 @@
+public class Personaje { 
+private String nom; 
+private int dinero; 
+public Personaje(String nom, int dinero) { 
+this.nom = nom; 
+this.dinero = dinero; 
+} 
+public void mostrarInfo() { 
+System.out.println(nom + " tiene " + dinero + "$."); 
+} 
+public static void main(String[] args) { 
+Personaje jugador = new Personaje("Trevor", 500); 
+jugador.mostrarInfo(); 
+} 
+}
